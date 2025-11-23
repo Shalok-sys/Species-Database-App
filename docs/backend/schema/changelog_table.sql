c77aa168c9a276849a9438f8f4973a5ac3fe6f98
ZB1234D <165355165+ZB1234D@users.noreply.github.com>
2025-11-23 22:53:15 +0300

    Update changelog_table.sql
    fix typo

--- docs/backend/schema/changelog_table.sql
+++ docs/backend/schema/changelog_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE changelog (
   change_id SERIAL PRIMARY KEY,
   version INT NOT NULL,
-  species_id UUID REFRENCES species(id) ON DELETE CASCADE,
+  species_id UUID REFERENCES species(id) ON DELETE CASCADE,
   operation TEXT CHECK (operation IN ('create', 'update', 'delete')),
   payload JSONB,
   created_at TIMESTAMP DEFAULT now()
